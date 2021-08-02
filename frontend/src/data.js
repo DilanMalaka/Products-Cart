dc48dc188655d133d22e9c3b8837da42995aabb8
DilanMalaka <malakanawarathna280@gmail.com>
2021-08-02 16:29:02 +0530

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,74 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Chopsey Rice',
+            category: 'Rice',
+            image: '/images/f1.jpg',
+            price: 120,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '2',
+            name: 'Chopsey Rice 2',
+            category: 'Rice',
+            image: '/images/f1.jpg',
+            price: 120,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '3',
+            name: 'Chopsey Rice',
+            category: 'Rice',
+            image: '/images/f1.jpg',
+            price: 120,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '4',
+            name: 'Chopsey Rice',
+            category: 'Rice',
+            image: '/images/f1.jpg',
+            price: 120,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '5',
+            name: 'Chopsey Rice',
+            category: 'Rice',
+            image: '/images/f1.jpg',
+            price: 120,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '6',
+            name: 'Chopsey Rice 6',
+            category: 'Rice',
+            image: '/images/f1.jpg',
+            price: 120,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+
+
+    ],
+};
+
+export default data;
